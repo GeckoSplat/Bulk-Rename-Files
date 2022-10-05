71178eaccbcbad02d368a599a9a96d49d82a0d16
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2022-10-05 11:05:11 +0100

    Cancel button on second window now working properly

--- Main.py
+++ Main.py
@@ -1,26 +1,51 @@
 
 import os
+from tkinter import *
 from tkinter import Tk, simpledialog
 from tkinter.filedialog import askdirectory
 
 
-Tk().withdraw()
 
-path = askdirectory (title = 'Please select a Folder to Bulk Rename Files')
+Tk().withdraw()
+path = askdirectory (title = 'Please select a Folder to Bulk Rename contained Files')
 walker2 = os.walk(path)
- 
-user_input = simpledialog.askstring(title ='Enter Name for Files', prompt ='Files will be named:')
+
 
 def rename():
     path 
     for count, filename in enumerate (os.listdir(path)):
-        dst = f"{path}/{user_input}{count}"
+        dst = f"{path}/{T1.get()}{count}"
         src = f"{path}/{filename}"
         
         os.rename(src,dst)
 
 
-if __name__ == '__main__':
+def OK_button_setup(tk):
+    print('OK pressed')
+    Button(text = "OK")
     rename()
+    exit()
+    
 
-    ## need to make cancel button exit () code , not tkinter mainloop.
+def Cancel_button_setup(tk): 
+    print ('Cancel pressed')   
+    Button (text = "CANCEL")
+    exit()
+
+
+top = Tk()
+top.geometry('400x200')
+top.title("Enter Desired Name for Files")
+B1 = Button(top,text ="OK")
+B2 = Button(top,text = "Cancel")
+Title =Label(top,text="Enter Name", fg='black')
+T1=Entry(top, fg='red', bd=3)
+T1.get()
+Title.pack(fill=X, padx=10)
+T1.pack(fill=X, padx=10)
+B1.pack(fill=X, padx=10)
+B1.bind("<Button->", OK_button_setup)
+B2.pack(fill=X, padx=10)
+B2.bind("<Button->", Cancel_button_setup)
+top.mainloop()
+    
